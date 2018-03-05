f52220aa72e52c3cac716b412ac984bec614e4bf
iliagerman <iliagerman@gmail.com>
2018-03-05 09:45:05 +0200

    Changed message

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ var app = express();
 var port = 3000;
 var primes = require('./primes.js');
 
-app.get('/', function(req, res){
-    res.status(200).send('Hello World!');
+app.get('/', function(req, res) {
+    res.status(200).send('Cowabunga!!');
 })
 
-app.get('/isPrime/:number', function(req, res){
-    res.status(200).send(primes.isPrime(req.params.number));    
+app.get('/isPrime/:number', function(req, res) {
+    res.status(200).send(primes.isPrime(req.params.number));
 })
 
-exports.stop = function(){
+exports.stop = function() {
     server.close();
 }
-var server = app.listen(port, function(){
-  console.log("Express server listening on port %d in %s mode", port, app.settings.env);
+var server = app.listen(port, function() {
+    console.log("Express server listening on port %d in %s mode", port, app.settings.env);
 });
