6eadf71df4958669c65d82e1e4152e59ca0ff390
germanilia <34738985+germanilia@users.noreply.github.com>
2018-03-05 19:47:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 3000;
 var primes = require('./primes.js');
 
 app.get('/', function(req, res) {
-    res.status(200).send('TMNT Rules');
+    res.status(200).send('TMNT Rules!');
 })
 
 app.get('/isPrime/:number', function(req, res) {
