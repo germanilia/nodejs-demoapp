effa52763d7a844cf9a913053fb5d50bfe009785
germanilia <34738985+germanilia@users.noreply.github.com>
2018-03-08 10:01:37 +0200

    Update primes-spec.js

--- spec/primes-spec.js
+++ spec/primes-spec.js
@@ -2,7 +2,7 @@ var sut = require('../primes.js');
 describe("when evaluating if a given number is a prime", function(){
   describe("when the number is a prime", function(){
     it("should return 'true'", function(){
-      expect(sut.isPrime(12)).toBe(true);
+      expect(sut.isPrime(11)).toBe(true);
     });
   });
 });
